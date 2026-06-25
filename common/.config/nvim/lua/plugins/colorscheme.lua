accbf5ce938c8c8c05469f43adac8da828fe0b4b
thienaang.dev <thienaang.dev@gmail.com>
2026-06-25 11:01:07 +0700

    feat(ui): Use Gruvbox-Material (Soft)

--- common/.config/nvim/lua/plugins/colorscheme.lua
+++ common/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,23 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    "sainnhe/gruvbox-material",
     lazy = false,
     priority = 1000,
-    --- @type GruvboxConfig
-    opts = {
-      contrast = "soft",
-      dim_inactive = true,
-      transparent_mode = true,
-    },
-    config = function(_, opts)
-      require("gruvbox").setup(opts)
-      vim.cmd.colorscheme("gruvbox")
+    config = function()
+      vim.g.gruvbox_material_background = "soft"
+      vim.g.gruvbox_material_transparent_background = 2
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_diagnostic_text_highlight = 1
+      vim.g.gruvbox_material_diagnostic_line_highlight = 1
+      vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+
+      vim.cmd.colorscheme("gruvbox-material")
     end,
   },
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "gruvbox" },
+    opts = { colorscheme = "gruvbox-material" },
   },
 
   -- Disable plugins
