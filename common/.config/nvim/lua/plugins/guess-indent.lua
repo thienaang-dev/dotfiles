edda1e0af41ccf9986eabc5cc5d0ff14fcec0c8d
thienaang.dev <thienaang.dev@gmail.com>
2026-07-16 17:27:31 +0700

    perf(nvim): Improve load time

--- common/.config/nvim/lua/plugins/guess-indent.lua
+++ common/.config/nvim/lua/plugins/guess-indent.lua
@@ -1,7 +1,10 @@
 return {
   "NMAC427/guess-indent.nvim",
   event = "BufReadPost",
-  config = function()
+  opts = {},
+  config = function(_, opts)
+    require("guess-indent").setup(opts)
+
     vim.keymap.set("n", "<leader>ri", "<cmd>GuessIndent<CR>", { desc = "Restart [I]ndent" })
   end,
 }
