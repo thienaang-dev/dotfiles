ac42e74abb6cea71374cc7d4fc2cd95920be806e
thienaang.dev <thienaang.dev@gmail.com>
2026-07-23 13:11:38 +0700

    feat(nvim): Add bufferline plugin

---
+++ common/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/bufferline.nvim",
+  event = "BufReadPost",
+  version = "*",
+  ---@module "bufferline"
+  ---@type bufferline.Options
+  opts = {
+    options = {
+      always_show_bufferline = false,
+    },
+  },
+}
