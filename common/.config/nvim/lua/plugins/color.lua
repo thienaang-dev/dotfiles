f2dcc02218d4e9b0357330bd195c5625e7c374f0
thienaang.dev <thienaang.dev@gmail.com>
2026-08-26 19:35:57 +0700

    feat(UI): Use Gruvbox Material (soft)

--- common/.config/nvim/lua/plugins/color.lua
+++ common/.config/nvim/lua/plugins/color.lua
@@ -1,23 +1,23 @@
 return {
   {
-    "sainnhe/everforest",
+    "sainnhe/gruvbox-material",
     lazy = false,
     priority = 1000,
     config = function()
       local g = vim.g
-      g.everforest_background = "soft"
-      g.everforest_enable_italic = 1
-      g.everforest_transparent_background = 2
-      g.everforest_diagnostic_text_highlight = 1
-      g.everforest_diagnostic_line_highlight = 1
-      g.everforest_diagnostic_virtual_text = "colored"
-      g.everforest_better_performance = 1
+      g.gruvbox_material_background = "soft"
+      g.gruvbox_material_enable_italic = 1
+      g.gruvbox_material_transparent_background = 2
+      g.gruvbox_material_diagnostic_text_highlight = 1
+      g.gruvbox_material_diagnostic_line_highlight = 1
+      g.gruvbox_material_diagnostic_virtual_text = "colored"
+      g.gruvbox_material_better_performance = 1
 
-      vim.cmd.colorscheme("everforest")
+      vim.cmd.colorscheme("gruvbox-material")
     end,
   },
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "everforest" },
+    opts = { colorscheme = "gruvbox-material" },
   },
 }
